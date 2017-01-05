8d09af00583ddab4943a47585e195e3975fd76b6
Charlie Cuddy <charliecuddy4@gmail.com>
2017-01-05 16:08:23 -0600

    Try-Catch example to help user understand potential errors

---
+++ HandlingExceptions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"D:\Coding\DotNet\Projects\HandlingExceptions\Example.txt");
+                Console.WriteLine(content);
+                
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the file is named correctly");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("Make sure the directory exists");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine(ex.Message);
+
+            }
+            finally
+            {
+                // Code to finalize
+                // Setting objects to null
+                // Closing database connections
+                Console.WriteLine("Closing application now...");
+            }
+            Console.ReadLine();
+        }
+    }
+}
